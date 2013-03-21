d426dd21f6110ee4764e30a5491e666457af5e80
Dirk Gadsden <dirk@esherido.com>
2013-03-21 14:10:42 -0400

    Tweaking the list block for deferred rendering

--- lib/burst/blocks/list.rb
+++ lib/burst/blocks/list.rb
@@ -1,23 +1,41 @@
 module Burst
   module Blocks
     class List < Basic
-      def initialize(list_type, element_list)
+      attr_accessor :elements, :parent, :type
+      def initialize(list_type, element_list = nil)
         @type = list_type
-        @elements = element_list
-        @elements.map! do |element|
-          parser = Burst::Parser.new
-          parser.parse(element)
+        
+        if element_list
+          @elements = element_list
+          @elements.map! do |element|
+            parser = Burst::Parser.new
+            parser.parse(element)
+          end
+        else
+          @elements = []
         end
       end
-
+      
       def to_html(renderer)
         html = "<ul>\n"
-
-        @elements.each do |element|
-          html << "<li>\n#{element.render}\n</li>\n"
+        render_item = Proc.new do |el|
+          if el.is_a? String
+            renderer.render(el)
+          else
+            el.to_html(renderer)
+          end
         end
-
-        html << "</ul>"
+        
+        @elements.each do |element|
+          # Handle sub-blocks
+          if element.is_a? Array
+            element = element.map {|el| render_item.call(el) }.join "\n"
+          else
+            element = render_item.call(element)
+          end
+          html << "<li>\n#{element}\n</li>\n"
+        end
+        html << "</ul>\n"
       end
     end
   end
